daa4fdbebcd81abb14b2b13823a7cc68819adf28
Dan Zaharia <danzaharia@Dans-Mac-mini.local>
2025-02-18 22:13:04 -0500

    make review apps work

--- vite.config.js
+++ vite.config.js
@@ -50,6 +50,6 @@ export default defineConfig({
   ],
   server: {
     port: process.env.VITE_PORT || 3001,
-    allowedHosts: ['spanish.danzaharia.com', '.danzaharia.com', 'all']
+    allowedHosts: ['*']
   }
 })
