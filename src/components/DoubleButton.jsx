294bd51b9da529ab02d80cd118716f05273155e1
Dan Zaharia <zaharia.danny@gmail.com>
2026-02-26 14:32:08 -0500

    Fix double button corner radii issue

--- src/components/DoubleButton.jsx
+++ src/components/DoubleButton.jsx
@@ -6,11 +6,11 @@ const DoubleButtonContainer = styled.div`
   display: flex;
   gap: ${({ theme }) => theme.spacing.xSmall};
 
-  button:first-child {
+  div:first-child button {
     border-radius: 12px 6px 6px 12px;
   }
 
-  button:last-child {
+  div:last-child button {
     border-radius: 6px 12px 12px 6px;
   }
 `;
